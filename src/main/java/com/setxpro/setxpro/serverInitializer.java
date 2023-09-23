a9d4cc02dc6d3a661b22efaf341da94769544f23
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-09-23 13:13:35 -0300

    feat: config server

---
+++ src/main/java/com/setxpro/setxpro/serverInitializer.java
@@ -0,0 +1,12 @@
+package com.setxpro.setxpro;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class serverInitializer  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RegisterStockApplication.class);
+    }
+}
