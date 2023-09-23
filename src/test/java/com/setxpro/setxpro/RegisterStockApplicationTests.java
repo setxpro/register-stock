e6b06678e19dd56ef48fb9d6e9ba48310369948d
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-09-23 13:15:51 -0300

    test

---
+++ src/test/java/com/setxpro/setxpro/RegisterStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setxpro.setxpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
