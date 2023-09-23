1be54a36739aaf5b5fc67200a3d6435107f33704
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-09-23 13:15:33 -0300

    feat: config

---
+++ src/main/java/com/setxpro/setxpro/RegisterStockApplication.java
@@ -0,0 +1,13 @@
+package com.setxpro.setxpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterStockApplication.class, args);
+	}
+
+}
