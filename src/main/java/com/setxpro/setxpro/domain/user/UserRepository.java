cd6760c68bec41e66d5d8be1fc6161578b19c6ab
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-09-23 19:45:39 -0300

    feat: create interface UserRepository

---
+++ src/main/java/com/setxpro/setxpro/domain/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.setxpro.setxpro.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
